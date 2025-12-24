2b1a316fbdb8372ecf8b3f0c89bb0b4774bdc9ad
solid-tetianamovlian <tetiana.movlian@solid.software>
2025-12-24 12:07:41 +0200

    Kek

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_codemagic/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test 1', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test 2', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
